2e1f2800c1c208f269b71f31c6031490be56a381
Leah Irwin <irwin.leah@outlook.com>
2025-03-07 10:44:49 -0500

    cats added

--- main_app/views.py
+++ main_app/views.py
@@ -5,6 +5,26 @@ from django.shortcuts import render
 # Import HttpResponse to send text-based responses
 from django.http import HttpResponse
 
+# views.py
+
+class Cat:
+    def __init__(self, name, breed, description, age):
+        self.name = name
+        self.breed = breed
+        self.description = description
+        self.age = age
+
+# Create a list of Cat instances
+cats = [
+    Cat('Lolo', 'tabby', 'Kinda rude.', 3),
+    Cat('Sachi', 'tortoiseshell', 'Looks like a turtle.', 0),
+    Cat('Fancy', 'bombay', 'Happy fluff ball.', 4),
+    Cat('Bonk', 'selkirk rex', 'Meows loudly.', 6)
+]
+
+def cat_index(request):
+    return render(request, 'cats/index.html', {'cats': cats})
+
 # Define the home view function
 def home(request):
     # Send a simple HTML response
