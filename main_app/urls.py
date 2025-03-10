2c16ba84378f2ab8ca0da87bcac120601b4eb0bb
Leah Irwin <irwin.leah@outlook.com>
2025-03-10 11:18:14 -0400

    feedings and not fed warning working

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,22 +1,15 @@
 from django.urls import path
-# import all the functions in views file
-# as methods on a views object
+
 from . import views
 
 urlpatterns = [
-    # localhost:8000, 
-    # in the views folder we have home function
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('cats/', views.cat_index, name='cats-index'),
-    # cat_id is the name of our param
-    path('cats/<int:cat_id>/', views.cat_detail, name='cat-detail'),
-    # allow render CBV's as_view()
+    path('cats/<int:cat_id>/', views.cat_detail, name='cats-detail'),
     path('cats/create/', views.CatCreate.as_view(), name='cat-create'),
-    # CBV expect the params name to be pk (primary key)
     path('cats/<int:pk>/update/', views.CatUpdate.as_view(), name='cat-update'),
     path('cats/<int:pk>/delete/', views.CatDelete.as_view(), name='cat-delete'),
-    # 1 to many creation of a feeding (handling the post request from the feeding form)
-    # path('cats/<int:cat_id>/add_feeding/', views.add_feeding, name='add-feeding'),
-     
+    path('cats/<int:cat_id>/add_feeding/', views.add_feeding, name='add-feeding'),
+
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -2,9 +2,10 @@
 
 from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from .models import Cat
-# Import HttpResponse to send text-based responses
 from django.http import HttpResponse
+from .models import Cat
+from .forms import FeedingForm
+# Import HttpResponse to send text-based responses
 
 
 class CatUpdate(UpdateView):
@@ -34,4 +35,17 @@ def about(request):
 
 def cat_detail(request, cat_id):
     cat = Cat.objects.get(id=cat_id)
-    return render(request, 'cats/detail.html', {'cat': cat})
+    feeding_form = FeedingForm()
+    return render(request, 'cats/detail.html', {
+        'cat': cat, 'feeding_form': feeding_form
+    })
+
+def add_feeding(request, cat_id):
+    form = FeedingForm(request.POST)
+
+    if form.is_valid():
+
+        new_feeding = form.save(commit=False)
+        new_feeding.cat_id = cat_id
+        new_feeding.save()
+    return redirect('cats-detail', cat_id=cat_id)
